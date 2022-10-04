6d3170b1a520d0f217241b89afe5da116d8bf160
Med-lemineHmd <leminlemin00@gmail.com>
2022-10-04 17:04:42 +0100

    Popular Places Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        'comfortaa': ['Comfortaa', 'cursive']
-      }
+        comfortaa: ["Comfortaa", "cursive"],
+        lora: ["lora", "serif"],
+        nunito: ["Nunito", "sans-serif"],
+        libre: ["Libre Baskerville", "serif"],
+        monte: ["Montserrat", "sans-serif"],
+        sans: ["Proxima Nova", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
